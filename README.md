67e4fc487a138ecd92f896a020b8c6257b181ca0
Priyadarshan Ghosh Hazra <priyadarshanghoshhazra908@gmail.com>
2024-02-10 23:27:47 +0530

    Files added

---
+++ README.md
@@ -0,0 +1,47 @@
+
+# TraxSmart Assignment
+It is a interactive web app which contains random data in table format which you can download in excel format.
+
+
+
+
+
+
+
+## Authors
+
+- [@Priyadarshan](https://www.github.com/https://github.com/Priyadarshan-coder)
+
+
+
+
+
+
+
+
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone https://github.com/Priyadarshan-coder/ReacttoExcel.git
+```
+
+Go to the project directory
+
+```bash
+  cd ReactTable
+```
+
+Install dependencies
+
+```bash
+  npm install
+```
+
+Start the server
+
+```bash
+  npm run dev
+```
+
---
+++ src/App.jsx
@@ -0,0 +1,112 @@
+import React, { useState, useEffect, useRef } from "react";
+import axios from "axios";
+import { DownloadTableExcel } from 'react-export-table-to-excel';
+
+const App = () => {
+  const [users, setUsers] = useState([]);
+  const myRef = useRef(null);
+
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        setUsers(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
+  return (
+    <>
+      <div className="flex flex-col">
+        <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="py-4 inline-block min-w-full sm:px-6 lg:px-8">
+            <div className="overflow-hidden">
+              <table className="min-w-full text-center" ref={myRef}>
+                <thead className="border-b bg-gray-800">
+                  <tr>
+                    <th
+                      scope="col"
+                      className="text-sm font-medium text-white px-6 py-4"
+                    >
+                      #
+                    </th>
+                    <th
+                      scope="col"
+                      className="text-sm font-medium text-white px-6 py-4"
+                    >
+                      Name
+                    </th>
+                    <th
+                      scope="col"
+                      className="text-sm font-medium text-white px-6 py-4"
+                    >
+                      Username
+                    </th>
+                    <th
+                      scope="col"
+                      className="text-sm font-medium text-white px-6 py-4"
+                    >
+                      Phone
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {users.map((itm, k) => (
+                    <tr className="bg-white border-b" key={k}>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                        {itm.id}
+                      </td>
+                      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        {itm.name}
+                      </td>
+                      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        {itm.username}
+                      </td>
+                      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        {itm.phone}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+      <DownloadTableExcel
+        filename="users table"
+        sheet="users"
+        currentTableRef={myRef.current}
+      >
+        <div className="flex space-x-2 justify-center">
+          <div>
+            <button
+              type="button"
+              className="inline-block px-6 pt-2.5 pb-2 bg-gray-800 text-white font-medium text-xs leading-normal uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out flex align-center"
+            >
+              <svg
+                aria-hidden="true"
+                focusable="false"
+                data-prefix="fas"
+                data-icon="download"
+                className="w-3 mr-2"
+                role="img"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+              >
+                <path
+                  fill="currentColor"
+                  d="M216 0h80c13.3 0 24 10.7 24 24v168h87.7c17.8 0 26.7 21.5 14.1 34.1L269.7 378.3c-7.5 7.5-19.8 7.5-27.3 0L90.1 226.1c-12.6-12.6-3.7-34.1 14.1-34.1H192V24c0-13.3 10.7-24 24-24zm296 376v112c0 13.3-10.7 24-24 24H24c-13.3 0-24-10.7-24-24V376c0-13.3 10.7-24 24-24h146.7l49 49c20.1 20.1 52.5 20.1 72.6 0l49-49H488c13.3 0 24 10.7 24 24zm-124 88c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20zm64 0c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20z"
+                ></path>
+              </svg>
+              Download
+            </button>
+          </div>
+        </div>
+      </DownloadTableExcel>
+    </>
+  );
+};
+
+export default App;
